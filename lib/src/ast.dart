fc427f73bdeeeba1c16527b20a72834e74647ea5
K. P. Krasiński-Sroka <kpsroka@users.noreply.github.com>
2024-02-23 11:18:31 +0100

    Tests fail, but code compiles :D

--- lib/src/ast.dart
+++ lib/src/ast.dart
@@ -10,45 +10,55 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-class FtlString implements FtlJunk, FtlIdentifier, FtlStringLiteral, FtlText, FtlCommentLine, FtlBlankBlock {
+class FtlString
+    implements
+        FtlJunk,
+        FtlIdentifier,
+        FtlStringLiteral,
+        FtlText,
+        FtlCommentLine,
+        FtlBlankBlock {
   final String content;
 
-  FtlString._(this.content) : assert(content != null);
+  FtlString._(this.content);
 
   static FtlString fromString(String content) {
-    assert(content != null);
     return FtlString._(content);
   }
 }
 
 class FtlText implements _FtlPatternElementCandidate {}
+
 class FtlJunk implements FtlResourceCandidate {}
+
 class FtlBlankBlock implements FtlResourceCandidate {}
+
 class FtlCommentLine {}
 
 class FtlIdentifier implements _FtlVariantKeyCandidate {}
 
 class FtlStringLiteral implements _FtlInlineExpressionCandidate {}
 
-class FtlNumberLiteral extends FtlString implements _FtlInlineExpressionCandidate, _FtlVariantKeyCandidate {
+class FtlNumberLiteral extends FtlString
+    implements _FtlInlineExpressionCandidate, _FtlVariantKeyCandidate {
   final num value;
 
-  FtlNumberLiteral(String content)
-      : this.value = num.parse(content),
-        super._(content);
+  FtlNumberLiteral(super.content)
+      : value = num.parse(content),
+        super._();
 }
 
 class FtlAttribute {
   final FtlIdentifier identifier;
   final FtlPattern pattern;
 
-  FtlAttribute({this.identifier, this.pattern});
+  FtlAttribute({required this.identifier, required this.pattern});
 }
 
 class FtlAttributeAccessor {
   final FtlIdentifier identifier;
 
-  FtlAttributeAccessor({this.identifier});
+  FtlAttributeAccessor({required this.identifier});
 }
 
 class FtlNamedArgument implements _FtlArgumentCandidate {
@@ -58,8 +68,7 @@ class FtlNamedArgument implements _FtlArgumentCandidate {
   final dynamic literal;
 
   FtlNamedArgument(this.identifier, this.literal)
-      : assert(identifier != null),
-        assert(literal is FtlNumberLiteral || literal is FtlStringLiteral);
+      : assert(literal is FtlNumberLiteral || literal is FtlStringLiteral);
 }
 
 class _FtlArgumentCandidate {}
@@ -67,54 +76,56 @@ class _FtlArgumentCandidate {}
 class FtlArgument {
   final _FtlArgumentCandidate argument;
 
-  FtlArgument({this.argument});
+  FtlArgument({required this.argument});
 }
 
 class _FtlVariantKeyCandidate {}
+
 class FtlVariantKey {
   final _FtlVariantKeyCandidate key;
 
-  FtlVariantKey({this.key});
+  FtlVariantKey({required this.key});
 }
 
 class FtlVariant {
   final FtlVariantKey variantKey;
   final FtlPattern pattern;
 
-  FtlVariant({this.variantKey, this.pattern});
+  FtlVariant({required this.variantKey, required this.pattern});
 }
 
 class FtlDefaultVariant {
   final FtlVariantKey variantKey;
   final FtlPattern pattern;
 
-  FtlDefaultVariant({this.variantKey, this.pattern});
+  FtlDefaultVariant({required this.variantKey, required this.pattern});
 }
-
 
 class FtlVariantList {
   final FtlDefaultVariant defaultVariant;
   final List<FtlVariant> variants;
 
-  FtlVariantList({this.defaultVariant, this.variants});
+  FtlVariantList({required this.defaultVariant, required this.variants});
 }
 
 class _FtlInlinePlaceableCandidate {}
 
-class FtlInlinePlaceable implements _FtlPatternElementCandidate, _FtlInlineExpressionCandidate {
+class FtlInlinePlaceable
+    implements _FtlPatternElementCandidate, _FtlInlineExpressionCandidate {
   final _FtlInlinePlaceableCandidate placeable;
 
-  FtlInlinePlaceable({this.placeable});
+  FtlInlinePlaceable({required this.placeable});
 }
 
 class FtlBlockPlaceable implements _FtlInlinePlaceableCandidate {}
 
 class _FtlInlineExpressionCandidate {}
 
-class FtlInlineExpression implements _FtlInlinePlaceableCandidate, _FtlArgumentCandidate {
+class FtlInlineExpression
+    implements _FtlInlinePlaceableCandidate, _FtlArgumentCandidate {
   final _FtlInlineExpressionCandidate expression;
 
-  FtlInlineExpression({this.expression});
+  FtlInlineExpression({required this.expression});
 }
 
 class _FtlPatternElementCandidate {}
@@ -122,32 +133,33 @@ class _FtlPatternElementCandidate {}
 class FtlPatternElement {
   final _FtlPatternElementCandidate element;
 
-  FtlPatternElement({this.element});
+  FtlPatternElement({required this.element});
 }
 
 class FtlPattern {
   final List<FtlPatternElement> patternElements;
 
-  FtlPattern({this.patternElements});
+  FtlPattern({required this.patternElements});
 }
 
 class FtlSelectExpression implements _FtlInlinePlaceableCandidate {
   FtlInlineExpression inlineExpression;
   FtlVariantList variantList;
 
-  FtlSelectExpression({FtlInlineExpression this.inlineExpression, FtlVariantList this.variantList});
+  FtlSelectExpression(
+      {required this.inlineExpression, required this.variantList});
 }
 
 class FtlArgumentList {
   final List<FtlArgument> arguments;
 
-  FtlArgumentList({this.arguments});
+  FtlArgumentList({required this.arguments});
 }
 
 class FtlCallArguments {
   final FtlArgumentList arguments;
 
-  FtlCallArguments({this.arguments});
+  FtlCallArguments({required this.arguments});
 }
 
 class FtlTermReference implements _FtlInlineExpressionCandidate {
@@ -155,27 +167,31 @@ class FtlTermReference implements _FtlInlineExpressionCandidate {
   final FtlAttributeAccessor attributeAccessor;
   final FtlCallArguments callArguments;
 
-  FtlTermReference({this.identifier, this.attributeAccessor, this.callArguments});
+  FtlTermReference(
+      {required this.identifier,
+      required this.attributeAccessor,
+      required this.callArguments});
 }
 
 class FtlMessageReference implements _FtlInlineExpressionCandidate {
   final FtlIdentifier identifier;
   final FtlAttributeAccessor attributeAccessor;
 
-  FtlMessageReference({this.identifier, this.attributeAccessor});
+  FtlMessageReference(
+      {required this.identifier, required this.attributeAccessor});
 }
 
 class FtlFunctionReference implements _FtlInlineExpressionCandidate {
   final FtlIdentifier identifier;
   final FtlCallArguments callArguments;
 
-  FtlFunctionReference({this.identifier, this.callArguments});
+  FtlFunctionReference({required this.identifier, required this.callArguments});
 }
 
 class FtlVariableReference implements _FtlInlineExpressionCandidate {
   final FtlIdentifier identifier;
 
-  FtlVariableReference({this.identifier});
+  FtlVariableReference({required this.identifier});
 }
 
 class FtlTerm {
@@ -183,27 +199,32 @@ class FtlTerm {
   final FtlPattern pattern;
   final List<FtlAttribute> attributes;
 
-  FtlTerm({this.identifier, this.pattern, this.attributes});
+  FtlTerm(
+      {required this.identifier,
+      required this.pattern,
+      required this.attributes});
 }
 
 class FtlMessage {
   final FtlIdentifier identifier;
-  final FtlPattern pattern;
+  final FtlPattern? pattern;
   final List<FtlAttribute> attributes;
 
-  FtlMessage({this.identifier, this.pattern, this.attributes});
+  FtlMessage(
+      {required this.identifier, this.pattern, required this.attributes});
 }
 
 class FtlEntry implements FtlResourceCandidate {
-  final FtlMessage message;
-  final FtlTerm term;
-  final FtlCommentLine commentLine;
+  final FtlMessage? message;
+  final FtlTerm? term;
+  final FtlCommentLine? commentLine;
 
   FtlEntry({this.message, this.term, this.commentLine});
 
   factory FtlEntry.forMessage(FtlMessage message) => FtlEntry(message: message);
   factory FtlEntry.forTerm(FtlTerm term) => FtlEntry(term: term);
-  factory FtlEntry.forCommentLine(FtlCommentLine commentLine) => FtlEntry(commentLine: commentLine);
+  factory FtlEntry.forCommentLine(FtlCommentLine commentLine) =>
+      FtlEntry(commentLine: commentLine);
 }
 
 class FtlResourceCandidate {}
@@ -211,5 +232,5 @@ class FtlResourceCandidate {}
 class FtlResource {
   final List<FtlResourceCandidate> resourceParts;
 
-  FtlResource({this.resourceParts});
+  FtlResource({required this.resourceParts});
 }
--- lib/src/parser.dart
+++ lib/src/parser.dart
@@ -12,99 +12,229 @@
 
 library ftl;
 
-import 'package:ftl/ftl.dart';
-
-import './ast.dart';
 import 'package:petitparser/petitparser.dart';
 
-Parser _setUpParser() {
-  Parser<FtlString> _line_end = (string('\u000D\u000A') | string('\u000A')).flatten().map(FtlString.fromString);
-  Parser<FtlString> _blank_inline = char('\u0020').plus().flatten().map(FtlString.fromString);
-  Parser<FtlBlankBlock> _blank_block = (_blank_inline & _line_end).plus().flatten().map(FtlString.fromString);
-  Parser<FtlString> _blank = (_blank_inline | _line_end).plus().flatten().map(FtlString.fromString);
+import './ast.dart';
 
-  Parser<FtlString> _junk_line = (_line_end.neg().star() & _line_end).flatten().map(FtlString.fromString);
-  Parser<FtlJunk> _junk = (_junk_line & ((letter() | char('#') | char('-')).not() & _junk_line).star()).flatten().map(FtlString.fromString);
+Parser<FtlResource> _setUpParser() {
+  Parser<FtlString> lineEnd = (string('\u000D\u000A') | string('\u000A'))
+      .flatten()
+      .map(FtlString.fromString);
+  Parser<FtlString> blankInline =
+      char('\u0020').plus().flatten().map(FtlString.fromString);
+  Parser<FtlBlankBlock> blankBlock =
+      (blankInline & lineEnd).plus().flatten().map(FtlString.fromString);
+  Parser<FtlString> blank =
+      (blankInline | lineEnd).plus().flatten().map(FtlString.fromString);
 
-  Parser<FtlString> _special_text_char = (char('{') | char('}')).flatten().map(FtlString.fromString);
-  Parser<FtlString> _text_char = ((_special_text_char.or(_line_end)).not() & any()).flatten().map(FtlString.fromString);
-  Parser<FtlString> _indented_char = ((char('.').or(char('*')).or(char('['))).not() & _text_char).flatten().map(FtlString.fromString);
-  Parser<FtlString> _special_quoted_char = (char('"') | char('\\')).flatten().map(FtlString.fromString);
-  Parser<FtlString> _special_escape = (char('\\') & _special_quoted_char).flatten().map(FtlString.fromString);
-  Parser<FtlString> _unicode_escape_4 = (string('\\u') & word().times(4)).flatten().map(FtlString.fromString);
-  Parser<FtlString> _unicode_escape_6 = (string('\\U') & word().times(6)).flatten().map(FtlString.fromString);
-  Parser<FtlString> _unicode_escape = (_unicode_escape_4 | _unicode_escape_6).map((dynamic raw) => raw);
-  Parser<FtlString> _quoted_char = (_text_char | _special_escape | _unicode_escape).map((dynamic raw) => raw);
+  Parser<FtlString> junkLine =
+      (lineEnd.neg().star() & lineEnd).flatten().map(FtlString.fromString);
+  Parser<FtlJunk> junk =
+      (junkLine & ((letter() | char('#') | char('-')).not() & junkLine).star())
+          .flatten()
+          .map(FtlString.fromString);
 
-  Parser<FtlText> _inline_text = _text_char.plus().flatten().map(FtlString.fromString);
-  Parser<FtlText> _block_text = (_blank_block & _blank_inline & _indented_char & _inline_text.optional()).flatten().map(FtlString.fromString);
+  Parser<FtlString> specialTextChar =
+      (char('{') | char('}')).flatten().map(FtlString.fromString);
+  Parser<FtlString> textChar = ((specialTextChar.or(lineEnd)).not() & any())
+      .flatten()
+      .map(FtlString.fromString);
+  Parser<FtlString> indentedChar =
+      ((char('.').or(char('*')).or(char('['))).not() & textChar)
+          .flatten()
+          .map(FtlString.fromString);
+  Parser<FtlString> specialQuotedChar =
+      (char('"') | char('\\')).flatten().map(FtlString.fromString);
+  Parser<FtlString> specialEscape =
+      (char('\\') & specialQuotedChar).flatten().map(FtlString.fromString);
+  Parser<FtlString> unicodeEscape4 =
+      (string('\\u') & word().times(4)).flatten().map(FtlString.fromString);
+  Parser<FtlString> unicodeEscape6 =
+      (string('\\U') & word().times(6)).flatten().map(FtlString.fromString);
+  Parser<FtlString> unicodeEscape =
+      (unicodeEscape4 | unicodeEscape6).map((dynamic raw) => raw);
+  Parser<FtlString> quotedChar =
+      (textChar | specialEscape | unicodeEscape).map((dynamic raw) => raw);
 
-  Parser<FtlString> _comment_char = (_line_end.not() & any()).flatten().map(FtlString.fromString);
-  Parser<FtlCommentLine> _comment_line = ((string('###') | string('##') | string('#')) & (char(' ') & _comment_char.star()).optional([]) & _line_end).flatten().map(FtlString.fromString);
+  Parser<FtlText> inlineText =
+      textChar.plus().flatten().map(FtlString.fromString);
+  Parser<FtlText> blockText =
+      (blankBlock & blankInline & indentedChar & inlineText.optional())
+          .flatten()
+          .map(FtlString.fromString);
 
-  Parser<FtlIdentifier> _identifier = (letter() & (word() | char('_') | char('-')).star()).flatten().map(FtlString.fromString);
-  Parser<FtlNumberLiteral> _number_literal = ((char('-').optional('')) & digit().plus() & (char('.') & digit().plus()).optional([''])).flatten().map((String content) => FtlNumberLiteral(content));
-  Parser<FtlStringLiteral> _string_literal = (char('"') & _quoted_char.star() & char('"')).pick<String>(1).map(FtlString.fromString);
+  Parser<FtlString> commentChar =
+      (lineEnd.not() & any()).flatten().map(FtlString.fromString);
+  Parser<FtlCommentLine> commentLine =
+      ((string('###') | string('##') | string('#')) &
+              (char(' ') & commentChar.star()).optional() &
+              lineEnd)
+          .flatten()
+          .map(FtlString.fromString);
 
-  SettableParser<FtlSelectExpression> _select_expression = undefined();
-  SettableParser<FtlInlineExpression> _inline_expression = undefined();
+  Parser<FtlIdentifier> identifier =
+      (letter() & (word() | char('_') | char('-')).star())
+          .flatten()
+          .map(FtlString.fromString);
+  Parser<FtlNumberLiteral> numberLiteral = ((char('-').optional()) &
+          digit().plus() &
+          (char('.') & digit().plus()).optional())
+      .flatten()
+      .map((String content) => FtlNumberLiteral(content));
+  Parser<FtlStringLiteral> stringLiteral =
+      (char('"') & quotedChar.star() & char('"'))
+          .pick(1)
+          .cast<String>()
+          .map(FtlString.fromString);
 
-  Parser<FtlInlinePlaceable> _inline_placeable =
-      (char('{') & _blank.optional() & (_select_expression | _inline_expression) & _blank.optional() & char('}')).pick(2).map((raw) => FtlInlinePlaceable(placeable: raw));
-  Parser<FtlBlockPlaceable> _block_placeable = (_blank_block & _blank_inline.optional() & _inline_placeable).pick(2);
+  SettableParser<FtlSelectExpression> selectExpression = undefined();
+  SettableParser<FtlInlineExpression> inlineExpression = undefined();
 
-  Parser<FtlPatternElement> _pattern_element = (_inline_text | _block_text | _inline_placeable | _block_placeable).map((raw) => FtlPatternElement(element: raw));
-  Parser<FtlPattern> _pattern = _pattern_element.plus().map((List<FtlPatternElement> elements) => FtlPattern(patternElements: elements));
-  Parser<FtlAttribute> _attribute = (_line_end & _blank.optional() & char('.') & _identifier & _blank_inline.optional() & char('=') & _blank_inline.optional() & _pattern)
-      .map((List<dynamic> raw) => FtlAttribute(identifier: raw[3], pattern: raw[7]));
+  Parser<FtlInlinePlaceable> inlinePlaceable = (char('{') &
+          blank.optional() &
+          (selectExpression | inlineExpression) &
+          blank.optional() &
+          char('}'))
+      .pick(2)
+      .map((raw) => FtlInlinePlaceable(placeable: raw));
+  Parser<FtlBlockPlaceable> blockPlaceable =
+      (blankBlock & blankInline.optional() & inlinePlaceable).pick(2).cast();
 
-  Parser<FtlNamedArgument> _named_argument =
-      (_identifier & (_blank.optional() & char(':') & _blank.optional()).flatten() & (_string_literal | _number_literal)).map((List<dynamic> parts) => FtlNamedArgument(parts[0], parts[2]));
-  Parser<FtlArgument> _argument = (_named_argument | _inline_expression).map((raw) => FtlArgument(argument: raw));
-  Parser<FtlArgumentList> _argument_list = ((_argument & _blank.optional() & char(',') & _blank.optional()).pick(0).star() & _argument.optional())
-      .map((List raw) => FtlArgumentList(arguments: List.from([...raw[0], raw[1]].where((dynamic element) => element != null))));
+  Parser<FtlPatternElement> patternElement =
+      (inlineText | blockText | inlinePlaceable | blockPlaceable)
+          .map((raw) => FtlPatternElement(element: raw));
+  Parser<FtlPattern> pattern = patternElement.plus().map(
+      (List<FtlPatternElement> elements) =>
+          FtlPattern(patternElements: elements));
+  Parser<FtlAttribute> attribute = (lineEnd &
+          blank.optional() &
+          char('.') &
+          identifier &
+          blankInline.optional() &
+          char('=') &
+          blankInline.optional() &
+          pattern)
+      .map((List<dynamic> raw) =>
+          FtlAttribute(identifier: raw[3], pattern: raw[7]));
 
-  Parser<FtlCallArguments> _call_arguments = (char('(').trim(_blank, _blank) & _argument_list & char(')').trim(_blank)).pick(1).map((dynamic args) => FtlCallArguments(arguments: args));
-  Parser<FtlAttributeAccessor> _attribute_accessor = (char('.') & _identifier).pick(1).map((raw) => FtlAttributeAccessor(identifier: raw));
-  Parser<FtlTermReference> _term_reference = (char('-') & _identifier & _attribute_accessor.optional() & _call_arguments.optional())
-      .map((List<dynamic> raw) => FtlTermReference(identifier: raw[1], attributeAccessor: raw[2], callArguments: raw[3]));
-  Parser<FtlMessageReference> _message_reference = (_identifier & _attribute_accessor.optional()).map((List<dynamic> raw) => FtlMessageReference(identifier: raw[0], attributeAccessor: raw[1]));
-  Parser<FtlFunctionReference> _function_reference = (_identifier & _call_arguments).map((List<dynamic> raw) => FtlFunctionReference(identifier: raw[0], callArguments: raw[1]));
-  Parser<FtlVariableReference> _variable_reference = (char('\$') & _identifier).pick(1).map((dynamic identifier) => FtlVariableReference(identifier: identifier));
+  Parser<FtlNamedArgument> namedArgument = (identifier &
+          (blank.optional() & char(':') & blank.optional()).flatten() &
+          (stringLiteral | numberLiteral))
+      .map((List<dynamic> parts) => FtlNamedArgument(parts[0], parts[2]));
+  Parser<FtlArgument> argument = (namedArgument | inlineExpression)
+      .map((raw) => FtlArgument(argument: raw));
+  Parser<FtlArgumentList> argumentList =
+      ((argument & blank.optional() & char(',') & blank.optional())
+                  .pick(0)
+                  .star() &
+              argument.optional())
+          .map((List raw) => FtlArgumentList(
+              arguments: List.from([...raw[0], raw[1]]
+                  .where((dynamic element) => element != null))));
 
-  Parser<FtlVariantKey> _variant_key = (char('[') & _blank.optional() & (_number_literal | _identifier) & _blank.optional() & char(']')).pick(3).map((dynamic key) => FtlVariantKey(key: key));
-  Parser<FtlDefaultVariant> _default_variant =
-      (_line_end & _blank.optional() & char('*') & _variant_key & _blank_inline.optional() & _pattern).map((List<dynamic> raw) => FtlDefaultVariant(variantKey: raw[3], pattern: raw[5]));
-  Parser<FtlVariant> _variant = (_line_end & _blank.optional() & _variant_key & _blank_inline.optional() & _pattern).map((List<dynamic> raw) => FtlVariant(variantKey: raw[2], pattern: raw[4]));
-  Parser<FtlVariantList> _variant_list = (_variant.star() & _default_variant & _variant.star() & _line_end).map((List<dynamic> raw) => FtlVariantList(defaultVariant: raw[1], variants: raw[2]));
-  ;
+  Parser<FtlCallArguments> callArguments =
+      (char('(').trim(blank, blank) & argumentList & char(')').trim(blank))
+          .pick(1)
+          .map((dynamic args) => FtlCallArguments(arguments: args));
+  Parser<FtlAttributeAccessor> attributeAccessor = (char('.') & identifier)
+      .pick(1)
+      .map((raw) => FtlAttributeAccessor(identifier: raw));
+  Parser<FtlTermReference> termReference = (char('-') &
+          identifier &
+          attributeAccessor.optional() &
+          callArguments.optional())
+      .map((List<dynamic> raw) => FtlTermReference(
+          identifier: raw[1],
+          attributeAccessor: raw[2],
+          callArguments: raw[3]));
+  Parser<FtlMessageReference> messageReference =
+      (identifier & attributeAccessor.optional()).map((List<dynamic> raw) =>
+          FtlMessageReference(identifier: raw[0], attributeAccessor: raw[1]));
+  Parser<FtlFunctionReference> functionReference = (identifier & callArguments)
+      .map((List<dynamic> raw) =>
+          FtlFunctionReference(identifier: raw[0], callArguments: raw[1]));
+  Parser<FtlVariableReference> variableReference = (char('\$') & identifier)
+      .pick(1)
+      .map(
+          (dynamic identifier) => FtlVariableReference(identifier: identifier));
 
-  Parser<FtlTerm> _term =
-      (char('-') & _identifier & char('=').trim(_blank, _blank) & _pattern & _attribute.star()).map((List<dynamic> raw) => FtlTerm(identifier: raw[1], pattern: raw[3], attributes: raw[4]));
-  ;
-  Parser<FtlMessage> _message = (_identifier & char('=').trim(_blank_inline, _blank_inline) & ((_pattern & _attribute.star()) | _attribute.plus())).map((List<dynamic> raw) {
+  Parser<FtlVariantKey> variantKey = (char('[') &
+          blank.optional() &
+          (numberLiteral | identifier) &
+          blank.optional() &
+          char(']'))
+      .pick(3)
+      .map((dynamic key) => FtlVariantKey(key: key));
+  Parser<FtlDefaultVariant> defaultVariant = (lineEnd &
+          blank.optional() &
+          char('*') &
+          variantKey &
+          blankInline.optional() &
+          pattern)
+      .map((List<dynamic> raw) =>
+          FtlDefaultVariant(variantKey: raw[3], pattern: raw[5]));
+  Parser<FtlVariant> variant = (lineEnd &
+          blank.optional() &
+          variantKey &
+          blankInline.optional() &
+          pattern)
+      .map((List<dynamic> raw) =>
+          FtlVariant(variantKey: raw[2], pattern: raw[4]));
+  Parser<FtlVariantList> variantList =
+      (variant.star() & defaultVariant & variant.star() & lineEnd).map(
+          (List<dynamic> raw) =>
+              FtlVariantList(defaultVariant: raw[1], variants: raw[2]));
+
+  Parser<FtlTerm> term = (char('-') &
+          identifier &
+          char('=').trim(blank, blank) &
+          pattern &
+          attribute.star())
+      .map((List<dynamic> raw) =>
+          FtlTerm(identifier: raw[1], pattern: raw[3], attributes: raw[4]));
+  Parser<FtlMessage> message = (identifier &
+          char('=').trim(blankInline, blankInline) &
+          ((pattern & attribute.star()) | attribute.plus()))
+      .map((List<dynamic> raw) {
     assert(raw[2] is List && raw[2].isNotEmpty);
     if (raw[2][0] is FtlPattern) {
-      return FtlMessage(identifier: raw[0], pattern: raw[2][0], attributes: List.castFrom((raw[2]).sublist(1)));
+      return FtlMessage(
+          identifier: raw[0],
+          pattern: raw[2][0],
+          attributes: List.castFrom((raw[2]).sublist(1)));
     } else {
-      return FtlMessage(identifier: raw[0], attributes: raw[2] as List<FtlAttribute>);
+      return FtlMessage(
+          identifier: raw[0], attributes: raw[2] as List<FtlAttribute>);
     }
   });
-  Parser<FtlEntry> _entry = ((_message & _line_end).pick(0) | (_term & _line_end).pick(0) | _comment_line).map((raw) {
+
+  Parser<FtlEntry> entry =
+      ((message & lineEnd).pick(0) | (term & lineEnd).pick(0) | commentLine)
+          .map((raw) {
     if (raw is FtlMessage) return FtlEntry.forMessage(raw);
     if (raw is FtlTerm) return FtlEntry.forTerm(raw);
     if (raw is FtlCommentLine) return FtlEntry.forCommentLine(raw);
-    assert(false);
-    return null;
+    assert(false, 'Unsupported type ${raw.runtimeType}: $raw');
+    throw Exception('No fun');
   });
 
-  _select_expression.set((_inline_expression & (_blank.optional() & string('->') & _blank_inline.optional()).flatten() & _variant_list)
-      .map((List<dynamic> raw) => FtlSelectExpression(inlineExpression: raw[0], variantList: raw[2])));
-  _inline_expression.set(
-      (_string_literal | _number_literal | _function_reference | _message_reference | _term_reference | _variable_reference | _inline_placeable).map((raw) => FtlInlineExpression(expression: raw)));
+  selectExpression.set((inlineExpression &
+          (blank.optional() & string('->') & blankInline.optional()).flatten() &
+          variantList)
+      .map((List<dynamic> raw) =>
+          FtlSelectExpression(inlineExpression: raw[0], variantList: raw[2])));
+  inlineExpression.set((stringLiteral |
+          numberLiteral |
+          functionReference |
+          messageReference |
+          termReference |
+          variableReference |
+          inlinePlaceable)
+      .map((raw) => FtlInlineExpression(expression: raw)));
 
-  Parser<FtlResource> _resource = (_entry | _blank_block | _junk).star().map((List<dynamic> raw) => FtlResource(resourceParts: List.castFrom(raw)));
-  return _resource;
+  Parser<FtlResource> resource = (entry | blankBlock | junk).star().map(
+      (List<dynamic> raw) => FtlResource(resourceParts: List.castFrom(raw)));
+  return resource;
 }
 
 Parser<FtlResource> _resourceParser = _setUpParser();
